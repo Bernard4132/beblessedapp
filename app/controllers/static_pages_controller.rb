af6fda40e99daeb8521dee85bc4b79fc0f4d8ccb
bernard4132 <bernardlomo2@gmail.com>
2018-09-14 03:20:44 +0000

    new push

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,68 @@
 class StaticPagesController < ApplicationController
 
 def home
+	render layout: "landinglay"
 end
 
-def project_tl	
+def project_tl
+    render layout: "landinglay"	
 end
+
+def homeprop
+	
+end
+
+def aboutus
+	
+end
+
+def whoweare
+	
+end
+
+def whatwedo
+	
+end
+
+def howwegothere
+	
+end
+
+def boardofdirectors
+	
+end
+
+def oneoffdonation
+	
+end
+
+def monthlydonation
+	
+end
+
+def yearlydonation
+	
+end
+
+def ourprojects
+	
+end
+
+def news
+
+end
+
+def faqs
+	
+end
+
+def howtohelp
+	
+end
+
+def volunteer
+	
+end
+
 
 end
